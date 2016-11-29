8add8760ab18dbc01608b5032662f236232c4055
misterio <misterio92@gmail.com>
2016-11-29 16:38:53 +0300

    small fix

--- src/CreditPilot/Responses/FindPayResponse.php
+++ src/CreditPilot/Responses/FindPayResponse.php
@@ -7,11 +7,25 @@ class FindPayResponse extends AbstractResponse
 
     public function resultCode()
     {
+        if (
+            !isset($this->parsed()->payment) ||
+            !isset($this->parsed()->payment->result)
+        ) {
+            return null;
+        }
+
         return current($this->parsed()->payment->result->attributes()->resultCode);
     }
 
     public function resultDescription()
     {
+        if (
+            !isset($this->parsed()->payment) ||
+            !isset($this->parsed()->payment->result)
+        ) {
+            return null;
+        }
+
         return current($this->parsed()->payment->result->attributes()->resultDescription);
     }
 
